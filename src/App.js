27a9763d6c9791e52bb3a19115e10cf7c763cdf6
dodzikojo <dodzi@windowslive.com>
2023-03-21 23:01:00 +0000

    Add support for tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+// import appLogo from './public/assets/logo.png';
 import nav from './components/navbar';
 import './App.css';
 import 'holderjs';
@@ -14,18 +15,37 @@ import quote from './components/quote';
 function App() {
   return (
     <div className="App">
-    <quote/>
-      {/* <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>A product name</Card.Title>
-          <Card.Text>
-            Some main content text can go here, a product description for example
-          </Card.Text>
-          <Button variant="primary">View</Button>
-        </Card.Body>
-      </Card> */}
-      
+
+      <div className="flex justify-center flex-col m-auto h-screen">
+        <div className="bg-white w-1/2 mx-auto  p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+          <div>
+            <img src={logo} className="App-logo mx-auto" alt="logo" />
+          </div>
+          <div className="mt-5 text-4xl">
+            Life is like riding a bicycle. To keep your balance you must keep moving.
+          </div>
+
+          <div className="mt-5 text-xl">
+            Albert Einstein
+          </div>
+
+          <div className="mt-5 flex justify-center">
+            {/* <button className="bg-indigo-600 rounded-sm w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-color">Client</button> */}
+            <button
+              type="button"
+              class="inline-block rounded-full border-2 border-neutral-800 px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900"
+              data-te-ripple-init>
+              Previous
+            </button>
+            <button
+              type="button"
+              class="ml-2 inline-block rounded-full border-2 border-neutral-800 px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900"
+              data-te-ripple-init>
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
 
     </div>
   );
