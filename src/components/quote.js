4246460b38e2b686c759509deb77812cf042ba99
dodzikojo <dodzi@windowslive.com>
2023-03-21 22:04:02 +0000

    Added packages

---
+++ src/components/quote.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const quotes = require("success-motivational-quotes");
+console.log(quotes.getAllQuotes());
+
+// function quote(){
+//     return (
+        
+//     )
+// }
+
+// export default quote;
