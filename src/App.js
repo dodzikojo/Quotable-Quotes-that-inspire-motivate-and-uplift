fd5ab615c80644edea5dae4d19d35736c0ab6537
dodzikojo <dodzi@windowslive.com>
2023-03-21 23:12:04 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
               data-te-ripple-init>
               Previous
             </button>
+          
             <button
               type="button"
               class="ml-2 inline-block rounded-full border-2 border-neutral-800 px-6 pt-2 pb-[6px] text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900"
