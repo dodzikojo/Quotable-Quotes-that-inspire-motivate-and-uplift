a92cd1ef9a33958c0a65b4d59789294d2143e947
dodzikojo <dodzi@windowslive.com>
2023-03-21 19:15:49 +0000

    Add brand assets and necessary packages
    (cherry picked from commit b5851a87b2054fb8ca4750d228fc2195b049c439)

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+function navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container">
+                The quick brown fox jumps over the lazy dog.
+                    <a className="navbar-brand" href="#">
+                        
+                    </a>
+                </div>
+            </nav>
+        </div>
+
+    )
+}
+
+export default navbar;
