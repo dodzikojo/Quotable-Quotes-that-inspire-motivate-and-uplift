667df0e9ddaaa9c17274a8e6ca90d3e44d3554df
dodzikojo <dodzi@windowslive.com>
2023-03-22 06:53:49 +0000

    Update state

--- src/App.js
+++ src/App.js
@@ -12,34 +12,62 @@ import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 import quotes from 'success-motivational-quotes'
 
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      body: retrieveTodayQuote().body,
+      author: retrieveTodayQuote().author
+      
+    }
+  }
+
+   nextQuote() {
+    console.log("Next quote")
+    this.setState({
+      body: 'Sample Body',
+      author: "Sample Author"
+    })
+  }
+
+
+  render() {
+
+    return (
+
+      <div className="App">
+        <div className="flex justify-center flex-col m-auto h-screen">
+          <div className="bg-white w-1/2 mx-auto  p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+            <div>
+              <img src={logo} className="App-logo mx-auto mb-2" alt="logo" />
+            </div>
+            {allQuotesCategories.map(createCategoryButtons)}
+            <hr />
+
+            <Quotecomp body={this.state.body} author={this.state.author} />
+            {/* <div>{this.state.body}</div> */}
+
+            <div className="mt-5 flex justify-center">
+              <button onClick={previousQuote} ><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
+              <button onClick={() => this.nextQuote()} className='ml-2' ><FontAwesomeIcon icon={faCircleArrowRight} /></button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
 
 const allQuotes = quotes.getAllQuotes();
 const allQuotesCategories = quotes.getAllCategories();
-console.log(allQuotesCategories)
+console.log(allQuotesCategories);
 
-function App() {
-  return (
-    <div className="App">
-      <div className="flex justify-center flex-col m-auto h-screen">
-        <div className="bg-white w-1/2 mx-auto  p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-          <div>
-            <img src={logo} className="App-logo mx-auto mb-2" alt="logo" />
-          </div>
-          {allQuotesCategories.map(createCategoryButtons)}
-          <hr />
 
-          <Quotecomp body={retrieveTodayQuote().body} author={retrieveTodayQuote().author} />
 
-          <div className="mt-5 flex justify-center">
-            <button onClick={previousQuote} ><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
-            <button onClick={nextQuote} className='ml-2' ><FontAwesomeIcon icon={faCircleArrowRight} /></button>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  );
-}
 
 function createCategoryButtons(category) {
   return (
@@ -66,9 +94,7 @@ function retrieveTodayQuote() {
   return quotes.getTodaysQuote();
 }
 
-function nextQuote() {
-  console.log("Next quote")
-}
+
 
 function previousQuote() {
   console.log("Previous Quote")
