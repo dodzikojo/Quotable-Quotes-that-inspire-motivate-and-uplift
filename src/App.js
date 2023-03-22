ed841656e5c23b0ad910cb5c35909b32a89f0855
dodzikojo <dodzi@windowslive.com>
2023-03-22 22:30:35 +0000

    clean codebase

--- src/App.js
+++ src/App.js
@@ -1,80 +1,16 @@
 import logo from './logo.png';
-// import appLogo from './public/assets/logo.png';
-import nav from './components/navbar';
 import './App.css';
 import 'holderjs';
 import Quotecomp from './components/quote-comp';
 import Category from './components/category'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faClipboard } from '@fortawesome/free-solid-svg-icons'
 import { TwitterShareButton, TwitterIcon } from 'react-share';
 
 import quotes from 'success-motivational-quotes'
 
 import React, { Component, useState } from 'react';
-// import useState from "react"
-
-
-
-
-// let displayedQuote = {
-//   id: 0,
-//   body: todayQuote.body,
-//   author: todayQuote.by,
-// }
-// console.log(allQuotesCategories);
-
-// class App extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       body: displayedQuote.body,
-//       author: displayedQuote.author,
-//       totalQuotes: allQuotes.length
-
-//     }
-//   }
-
-
-
-//  retrieveTodayQuote() {
-//   return quotes.getTodaysQuote();
-// }
-
-
-
-
-
-// render() {
-
-//   return (
-
-//     <div className="App">
-//       <div className="flex justify-center flex-col m-auto h-screen">
-//         <div className="bg-white w-1/2 mx-auto  p-8 md:p-12 my-10 rounded-lg shadow-2xl">
-//           <div>
-//             <img src={logo} className="App-logo mx-auto mb-2" alt="logo" />
-//           </div>
-//           {allQuotesCategories.map(createCategoryButtons)}
-//           <hr />
-
-//           <Quotecomp body={this.state.body} author={this.state.author} />
-
-//           <div className="mt-4 flex justify-center">
-//             <button onClick={() => this.previousQuote()} ><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
-//             <div className='px-2'>1 of {this.state.totalQuotes}</div>
-//             <button onClick={() => this.nextQuote()} className='' ><FontAwesomeIcon icon={faCircleArrowRight} /></button>
-//           </div>
-//         </div>
-//       </div>
-
-//     </div>
-//   );
-// }
-// }
 
 function App() {
 
@@ -90,7 +26,6 @@ function App() {
     currentQuoteCounter: 0,
     allRetrievedQuotes: allQuotes,
     totalQuotes: allQuotes.length,
-
   });
 
   function retrieveTodayQuote() {
@@ -142,12 +77,6 @@ function App() {
 
           <Quotecomp body={quoteState.body} author={quoteState.author} />
 
-          {/* <div className="mt-4 flex justify-center">
-            <button onClick={previousQuote} ><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
-            <div className='px-2'>{quoteState.currentQuoteCounter} of {quoteState.totalQuotes}</div>
-            <button onClick={nextQuote} className='' ><FontAwesomeIcon icon={faCircleArrowRight} /></button>
-          </div> */}
-
           <div class="flow-root">
           <TwitterShareButton
               url={"https://dodzikojo.github.io/Quotable-Quotes-that-inspire-motivate-and-uplift/"}
@@ -166,33 +95,6 @@ function App() {
     </div>
   )
 }
-
-// function test(){
-//   console.log("test")
-
-// }
-
-// function getQuotesCount() {
-//   this.setState({
-//     totalQuotes: allQuotes.length
-//   })
-// }
-
-
-
-
-
-// function retrieveQuoteByAuthor(authorName) {
-//   allQuotes = quotes.getQuotesByAuthor(authorName);
-// }
-
-
-
-
-
-// function previousQuote() {
-//   console.log("Previous Quote")
-// }
 
 
 export default App;
