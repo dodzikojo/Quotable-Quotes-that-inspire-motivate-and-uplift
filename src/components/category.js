0466994276ebea91f7999cbf484c674f64256c46
dodzikojo <dodzi@windowslive.com>
2023-03-22 05:25:32 +0000

    Create category component

---
+++ src/components/category.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function category(){
+    return (
+        <button class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded-full">
+        Button
+      </button>
+    )
+}
+
+export default category;
